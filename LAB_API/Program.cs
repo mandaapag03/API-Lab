7fea1aabf0289032fbb5dba397bd167f9ff0a9a4
mandaapag03 <paganiamanda791@gmail.com>
2023-08-30 22:39:36 -0300

    Reset Program.cs

--- LAB_API/Program.cs
+++ LAB_API/Program.cs
@@ -1,26 +1,12 @@
-using LAB_API;
-using LAB_API.Interfaces;
-using LAB_API.Repository;
-using LAB_API.Repository.Context;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services); // calling ConfigureServices method
-
-builder.Services.AddCors(policy =>
-    policy.AddDefaultPolicy(p =>
-        p.WithOrigins("*").AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddControllers();
 
 var app = builder.Build();
-startup.Configure(app, builder.Environment); // calling Configure method
 
 // Configure the HTTP request pipeline.
-
-app.UseCors();
 
 app.UseAuthorization();
 
